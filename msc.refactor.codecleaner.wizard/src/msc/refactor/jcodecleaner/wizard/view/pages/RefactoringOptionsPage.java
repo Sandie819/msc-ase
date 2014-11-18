a7b798dcb99907694d6f0749c65b9a2c236a4e27
Sandie819 <sandie.mulligan@gmail.com>
2014-11-18 21:13:48 +0000

    Adding Progress Bar messages, and trying out preview screen on refactoring options page

--- msc.refactor.codecleaner.wizard/src/msc/refactor/jcodecleaner/wizard/view/pages/RefactoringOptionsPage.java
+++ msc.refactor.codecleaner.wizard/src/msc/refactor/jcodecleaner/wizard/view/pages/RefactoringOptionsPage.java
@@ -20,19 +20,22 @@ import msc.refactor.jcodecleaner.wizard.model.RefactoringOpportunitiesModel;
 import msc.refactor.jcodecleaner.wizard.model.WizardModel;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.ltk.ui.refactoring.UserInputWizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
@@ -48,71 +51,113 @@ public class RefactoringOptionsPage extends UserInputWizardPage {
 	private Map<Button, RefactoringEnum> refactoringCheckboxMap;
 	private RefactoringBuilder refactoringBuilder;
 	private MultipleRefactoring multipleRefactoring;
-	
+	private PreviewSubPanel previewSubPanel;
+
+	private Group headingGroup;
 	private Group refactoringOptionsGroup;
 	private Group metricResultsGroup;
-//	private Group fitnessFunctionGroup;
-	
+	private Group previewGroup;
+
+	private List<Metric> metrics;
+	private RefactoringOpportunitiesModel refactoringOpportunities;
 	private boolean fileSelected;
+	private Label classNameLabel;
 	
+	private static final Font DEFAULT_FONT = new Font(Display.getDefault(),"Arial", 9, SWT.BOLD );
+
 	public RefactoringOptionsPage(WizardController controller, 
 			MultipleRefactoring multipleRefactoring, boolean fileSelected) {
 		super("Refactoring options");
 		setMessage("Metric results, please select refactoring from given choices");
-		
+
 		this.controller = controller;
 		this.multipleRefactoring = multipleRefactoring;
 		this.fileSelected = fileSelected;
-		
+
 		refactoringCheckboxMap = new HashMap<Button, RefactoringEnum>();
 		refactoringBuilder = new RefactoringBuilder();
+		
 	}
 
 	@Override
 	public void createControl(Composite parent) {
 		composite = new Composite(parent, SWT.NONE);		
-		composite.setLayout(new GridLayout(1, true));		
+		composite.setLayout(new GridLayout(1, true));
 		
-//		fitnessFunctionGroup = new Group(composite, SWT.SHADOW_ETCHED_IN);
-//		fitnessFunctionGroup.setText("Overall Fitness Function");
-//		fitnessFunctionGroup.setLayout(new GridLayout(1, true));
-//		fitnessFunctionGroup.setLayoutData(new GridData(GridData.FILL_BOTH));	
+		headingGroup = new Group(composite, SWT.BORDER_DOT);
 		
 		metricResultsGroup = new Group(composite, SWT.SHADOW_ETCHED_IN);
-		metricResultsGroup.setText("Metric Results");			
+		metricResultsGroup.setText("Metric Results");	
+		metricResultsGroup.setFont(DEFAULT_FONT);
 		metricResultsGroup.setLayout(new GridLayout(1, false));
 		metricResultsGroup.setLayoutData(new GridData(GridData.FILL_BOTH));
-		
+
 		refactoringOptionsGroup = new Group(composite, SWT.SHADOW_ETCHED_IN);
 		refactoringOptionsGroup.setText("Refactoring Options Available");
+		refactoringOptionsGroup.setFont(DEFAULT_FONT);
 		refactoringOptionsGroup.setLayout(new GridLayout(4, true));
 		refactoringOptionsGroup.setLayoutData(new GridData(GridData.FILL_BOTH));
 		
+		previewGroup = new Group(composite, SWT.BORDER_DOT);
+		previewSubPanel = new PreviewSubPanel(previewGroup);
+		
 		setControl(composite);
 		setPageComplete(false);		
-		
+
 		if(fileSelected) {
 			onEnterPage();
 		}
 	}
-	
+
 	/**
 	 * Sets up the configuration for the refactoring options page
 	 */
 	public void onEnterPage() {
-		WizardModel model = controller.getModel();
-		IFile file = model.getIFile();
-		multipleRefactoring.removeRefactoring();
 
-		Analyser analyser = new Analyser();
-		List<Metric> metrics = analyser.analyseSelection(file);
-		RefactoringOpportunitiesModel refactoringOpportunities = analyser.identifyRefactoringOpportunities(file);
-		model.addFitnessFunctionCalculation(analyser.calculateFitnessFunction(refactoringOpportunities.getAvailableRefactorings()));
-		model.setRefactoringOpportunities(refactoringOpportunities);
+		final WizardModel model = controller.getModel();
 
-		createPanels(metrics, refactoringOpportunities.getAvailableRefactorings(), model.getFitnessFunctionCalulations());
+		try {
+			getContainer().run(true, true, new IRunnableWithProgress() {
+
+				@Override
+				public void run(IProgressMonitor monitor) {
+					try {
+						monitor.beginTask("Computing refactoring list: ", 4);
+
+						IFile file = model.getIFile();
+						multipleRefactoring.removeRefactoring();
+
+						monitor.subTask(" analysing class ");
+						Thread.sleep(500);
+
+						Analyser analyser = new Analyser();
+						setMetrics(analyser.analyseSelection(file));
+						monitor.subTask(" calculating metrics ");
+						Thread.sleep(500);
+
+						setRefactoringOpportunities(analyser.identifyRefactoringOpportunities(file));
+						monitor.subTask(" identifying possible refactorings");
+						Thread.sleep(500);
+
+						model.addFitnessFunctionCalculation(analyser.calculateFitnessFunction(refactoringOpportunities.getAvailableRefactorings()));
+						model.setRefactoringOpportunities(refactoringOpportunities);
+
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					monitor.worked(1);         
+					monitor.done();
+
+				}
+			}); 
+		} catch(Exception e) {
+
+		}
+		createPanels(getMetrics(), getRefactoringOpportunities().getAvailableRefactorings(), 
+				model.getFitnessFunctionCalulations());
 	}
-	
+
 	/**
 	 * On Entering the Re-factoring options page sets metrics, refactorings
 	 * and fitness function
@@ -123,41 +168,29 @@ public class RefactoringOptionsPage extends UserInputWizardPage {
 	 */
 	public void createPanels(List<Metric> metrics, Set<RefactoringEnum> refactorings,
 			LinkedList<Double> fitnessFunctionCalcs){
-		
-		//createFitnessFunctionPanel(fitnessFunctionCalcs);		
+
+		createHeadingPanel();		
 		createMetricResultPanel(metrics);
 		createRefactoringOptions(refactorings);
 
 		composite.layout();
 	}
 
-//	/**
-//	 * @param fitnessFunctionCalcs
-//	 */
-//	private void createFitnessFunctionPanel(LinkedList<Double> fitnessFunctionCalcs) {
-//		clearGroup(fitnessFunctionGroup);
-//		
-//		Label fitnessLabel = new Label(fitnessFunctionGroup, SWT.NONE);
-//		fitnessLabel.setText("Calculated Fitness functions");
-//		
-//		Group fitnessFunctionResultGroup = new Group(fitnessFunctionGroup, SWT.SHADOW_ETCHED_IN);		
-//		fitnessFunctionResultGroup.setLayout(new GridLayout(4, true));
-//		
-//		int index = 1;
-//		for(Double fitnessCalculation: fitnessFunctionCalcs) {
-//			Label resultLabel = new Label(fitnessFunctionResultGroup, SWT.NONE);
-//			resultLabel.setText("Phase "+index+ ": ");
-//			
-//			Text text = new Text(fitnessFunctionResultGroup, SWT.BORDER);
-//			text.setText(String.valueOf(fitnessCalculation));
-//			text.setEditable(false);
-//			index++;
-//		}
-//		
-//		Label lblHint = new Label(fitnessFunctionGroup, SWT.NONE);
-//		lblHint.setText("Zero is optimal, above zero means there is room for improvment \n"
-//				+ "Based on weighted values from the various metrics outlined below.");		
-//	}
+	/**
+	 * Class name panel
+	 */
+	private void createHeadingPanel() {			
+		if(classNameLabel == null) {
+			headingGroup.setLayout(new GridLayout(1, true));
+			headingGroup.setLayoutData(new GridData(GridData.FILL_BOTH));
+
+			IFile file = controller.getModel().getIFile();
+			classNameLabel = new Label(headingGroup, SWT.NONE);
+			classNameLabel.setText(file.getName());
+			classNameLabel.setFont(DEFAULT_FONT);
+			classNameLabel.setForeground(Display.getDefault().getSystemColor(SWT.COLOR_RED));
+		}
+	}
 
 	/**
 	 * Adds each metric to the group for holding metric results
@@ -166,15 +199,15 @@ public class RefactoringOptionsPage extends UserInputWizardPage {
 	 */
 	private void createMetricResultPanel(List<Metric> metrics) {
 		clearGroup(metricResultsGroup);
-		
+
 		Group metricGroup = new Group(metricResultsGroup, SWT.NONE);		
 		metricGroup.setLayout(new GridLayout(2, false));
-		
+
 		for(Metric metric: metrics) {
 			Label metricLabel = new Label(metricGroup, SWT.NONE);
 			metricLabel.setText(metric.getMetricFullName());
 			metricLabel.setToolTipText(metric.getMetricFullName());
-			
+
 			Text metricTextValue = new Text(metricGroup, SWT.NONE);
 			metricTextValue.setText(String.valueOf(metric.getMetricValue()));
 			metricTextValue.setEditable(false);
@@ -195,35 +228,24 @@ public class RefactoringOptionsPage extends UserInputWizardPage {
 	 */
 	private void createRefactoringOptions(Set<RefactoringEnum> refactorings) {
 		clearGroup(refactoringOptionsGroup);
-		
+
 		if(refactorings.isEmpty()){
 			Label noOptions = new Label(refactoringOptionsGroup, SWT.NONE);
 			noOptions.setText("No refactorings were identified for your selected class");
 		}
+
+		Group refactorOpts = new Group(refactoringOptionsGroup, SWT.NONE);		
+		refactorOpts.setLayout(new GridLayout(2, false));
 		
 		for(RefactoringEnum refactor: refactorings) {
-			Button checkBox = new Button(refactoringOptionsGroup, SWT.CHECK);
+			
+			Button checkBox = new Button(refactorOpts, SWT.CHECK);
 			checkBox.setText(refactor.getRefactoringName());			
 			refactoringCheckboxMap.put(checkBox, refactor);	 
-			
+
 			addCheckBoxListener(checkBox);
 		}
 	}
-	
-	private RowLayout getRowLayout() {
-		RowLayout rowLayout = new RowLayout();
-		rowLayout.wrap = false;
-		rowLayout.pack = false;
-		rowLayout.justify = true;
-		rowLayout.type = SWT.HORIZONTAL;
-		rowLayout.marginLeft = 5;
-		rowLayout.marginTop = 5;
-		rowLayout.marginRight = 5;
-		rowLayout.marginBottom = 5;
-		rowLayout.spacing = 5;
-		return rowLayout;
-	}
-
 
 	/**
 	 * Adds listener to the given checkBox
@@ -232,40 +254,43 @@ public class RefactoringOptionsPage extends UserInputWizardPage {
 	private void addCheckBoxListener(Button checkBox) {
 		checkBox.addSelectionListener(new SelectionAdapter() {
 			@Override
-		    public void widgetSelected(SelectionEvent e) {
+			public void widgetSelected(SelectionEvent e) {
 
 				IFile file = controller.getModel().getIFile();
 				ICompilationUnit iCompilationUnit = JavaCore.createCompilationUnitFrom(file);
 				multipleRefactoring.setfCompilationUnit(iCompilationUnit);
-				
-		        Button button = (Button) e.widget;
-		        boolean selected = button.getSelection();
-		        enableDisableButtons(selected, button);
-		        
-		        RefactoringEnum refactor = refactoringCheckboxMap.get(button);
-		        
-		        if(!selected) {
-		        	multipleRefactoring.removeRefactoring(refactor);		        
-		        } else {
-		        	if(refactor==RefactoringEnum.EXTRACT_METHOD) {
-		        		ExtractMethodRefactoring extractMethodRefactoring = refactoringBuilder.getExtractedMethodRefactoring(controller);
-		        		multipleRefactoring.addRefactoringsToBeDone(extractMethodRefactoring);
-		        	} else if(refactor==RefactoringEnum.EXTRACT_CLASS) {
-		        		Set<ExtractClassRefactoring> extractClassRefactorings = refactoringBuilder.getExtractedClassRefactoring(controller);		        		
-		        		for(ExtractClassRefactoring extractClassRefactoring: extractClassRefactorings) {
-		        			multipleRefactoring.addRefactoringsToBeDone(extractClassRefactoring);	
-		        		}
-		        	} else if(refactor==RefactoringEnum.MOVE_METHOD) {
-		        		Set<MoveMethodRefactoring> moveMethodRefactorings = refactoringBuilder.getMoveMethodRefactoring(controller);
-		        		for(MoveMethodRefactoring moveMethod: moveMethodRefactorings) {
-		        			multipleRefactoring.addRefactoringsToBeDone(moveMethod);
-		        		}
-		        	}
 
-		        }     		      
-		        setPageComplete(anyCheckBoxSelected());		  
-		        
-		    }
+				Button button = (Button) e.widget;
+				boolean selected = button.getSelection();
+				enableDisableButtons(selected, button);
+
+				RefactoringEnum refactor = refactoringCheckboxMap.get(button);
+
+				if(!selected) {
+					multipleRefactoring.removeRefactoring(refactor);		        
+				} else {
+					if(refactor==RefactoringEnum.EXTRACT_METHOD) {
+						ExtractMethodRefactoring extractMethodRefactoring = refactoringBuilder.getExtractedMethodRefactoring(controller);
+						multipleRefactoring.addRefactoringsToBeDone(extractMethodRefactoring);
+					} else if(refactor==RefactoringEnum.EXTRACT_CLASS) {
+						Set<ExtractClassRefactoring> extractClassRefactorings = refactoringBuilder.getExtractedClassRefactoring(controller);		        		
+						for(ExtractClassRefactoring extractClassRefactoring: extractClassRefactorings) {
+							multipleRefactoring.addRefactoringsToBeDone(extractClassRefactoring);	
+						}
+						
+						previewSubPanel.addPreviewForClassRefactoring(multipleRefactoring);
+						
+					} else if(refactor==RefactoringEnum.MOVE_METHOD) {
+						Set<MoveMethodRefactoring> moveMethodRefactorings = refactoringBuilder.getMoveMethodRefactoring(controller);
+						for(MoveMethodRefactoring moveMethod: moveMethodRefactorings) {
+							multipleRefactoring.addRefactoringsToBeDone(moveMethod);
+						}
+					}
+
+				}     		      
+				setPageComplete(anyCheckBoxSelected());		  
+
+			}
 
 			private void enableDisableButtons(boolean selected, Button button) {
 				for(Button checkBox: refactoringCheckboxMap.keySet()){
@@ -281,10 +306,12 @@ public class RefactoringOptionsPage extends UserInputWizardPage {
 				}
 			}
 
-			
+
 		});
+		
+		composite.layout();
 	}
-	
+
 	private boolean anyCheckBoxSelected() {
 		for(Button checkBox: refactoringCheckboxMap.keySet()){
 			if(!checkBox.isDisposed() && checkBox.getSelection()) {
@@ -293,11 +320,11 @@ public class RefactoringOptionsPage extends UserInputWizardPage {
 		}
 		return false;
 	}
-	
+
 	public boolean hasRefactoringOptions() {
 		return refactoringCheckboxMap.size()>0;
 	}
-	
+
 	@Override
 	public IWizardPage getPreviousPage() {
 		if(anyCheckBoxSelected()) {
@@ -305,6 +332,23 @@ public class RefactoringOptionsPage extends UserInputWizardPage {
 		} else {
 			return super.getPreviousPage();
 		}
-    }
+	}
+
+	public List<Metric> getMetrics() {
+		return metrics;
+	}
+
+	public void setMetrics(List<Metric> metrics) {
+		this.metrics = metrics;
+	}
+
+	public RefactoringOpportunitiesModel getRefactoringOpportunities() {
+		return refactoringOpportunities;
+	}
+
+	public void setRefactoringOpportunities(RefactoringOpportunitiesModel refactoringOpportunities) {
+		this.refactoringOpportunities = refactoringOpportunities;
+	}
+
 
 }
